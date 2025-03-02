179690ea82f10cd492a6eefec6d24cb5aa1119c4
sedomu <seguin@lilo.org>
2025-03-02 19:28:55 +0100

    Lightbox component: implementing next and previous behaviour (if i<0 then i last of the list and vice versa)

--- scripts/components/lightboxComponent.js
+++ scripts/components/lightboxComponent.js
@@ -3,6 +3,10 @@ class Lightbox {
         this.element = document.querySelector('.lightbox-bg');
         this.close = document.querySelector('.lightbox-modal__close');
         this.viewer = document.querySelector('.lightbox-modal__viewer');
+        this.previous = document.querySelector('.lightbox-modal__previous');
+        this.next = document.querySelector('.lightbox-modal__next');
+        this.previousE = null;
+        this.nextE = null;
         // this.close.addEventListener('click', this.closeModal.bind(this));
         this.close.addEventListener('click', () => {this.closeModal()});
     }
@@ -15,21 +19,57 @@ class Lightbox {
         this.element.style.display = 'block';
     }
 
-    handleOpenMedia(e){
-        const media = document.createElement(e.target.tagName);
-        media.src = e.target.src;
+    displayOpenMedia(media) {
         this.viewer.replaceChild(media, this.viewer.firstChild);
-        console.log(e.target.tagName.toLowerCase());
+        console.log(media.tagName.toLowerCase());
         this.openModal();
     }
 
-    openMedia(){
-        const medias = document.querySelectorAll(".media-card__media");
-
-        for (let i = 0; i < medias.length; i++) {
-            medias[i].addEventListener("click", this.handleOpenMedia.bind(this));
+    handleControls(i, medias){
+        if (this.previousE && this.nextE){
+            console.log("coucou toi le not null");
+            this.previous.removeEventListener('click', this.previousE);
+            this.next.removeEventListener('click', this.nextE);
         }
 
+        this.previousE = () => {this.handleOpenMedia(i > 0 ? i-1 : medias.length - 1, medias);};
+        this.nextE = () => {this.handleOpenMedia(i < medias.length - 1 ? i+1 : 0, medias);};
+        this.previous.addEventListener('click', this.previousE);
+        this.next.addEventListener('click', this.nextE);
+    }
 
+    handleOpenMedia(i, medias){
+        console.log(i, medias);
+        console.log(medias[i].tagName.toLowerCase());
+        console.log(medias[i].src);
+
+        const media = document.createElement(medias[i].tagName);
+        media.src = medias[i].src;
+        this.displayOpenMedia(media);
+
+        this.handleControls(i, medias);
+
+
+        // console.log("media 0:")
+        // console.log(medias[0]);
+        //
+        // const media = document.createElement(e.target.tagName);
+        // media.src = e.target.src;
+        //
+        // this.displayOpenMedia(media);
+        //
+        // const previousE = medias[i-1];
+        // const nextE = medias[i+1];
+        //
+        // this.previous.addEventListener('click', () => {this.displayOpenMedia(previousE, i-1, medias);});
+        // this.next.addEventListener('click', () => {this.displayOpenMedia(nextE, i+1, medias);});
+    }
+
+    openMedia(){
+        const medias = document.querySelectorAll(".media-card__media > *");
+
+        for (let i = 0; i < medias.length; i++) {
+            medias[i].addEventListener("click", () => {this.handleOpenMedia(i, medias)})
+        }
     }
 }
