8375c593d17262e212f57666a7898311e51f422c
sedomu <seguin@lilo.org>
2025-03-03 14:07:54 +0100

    Contact modal component: - input as requested in the brief (display into the console && without form controls)

--- scripts/components/contactModalComponent.js
+++ scripts/components/contactModalComponent.js
@@ -9,6 +9,9 @@ class ContactModal{
         this.openContactButton.addEventListener("click", ()=>{this.openModal()});
         this.closeModalButton = document.querySelector(".contact-modal__close");
         this.closeModalButton.addEventListener("click", ()=>{this.closeModal()});
+
+        this.sendFormButton = document.querySelector(".contact_button");
+        this.sendFormButton.addEventListener("click", (e)=>{this.sendForm(e)});
     }
 
     openModal(){
@@ -25,5 +28,33 @@ class ContactModal{
         this.mainPage.style.top = "";
     }
 
+    sendForm(e){
+        // prevent form's default behaviour
+        e.preventDefault();
 
+        // forms elements as variables
+        let firstname = document.getElementById("firstname");
+        let lastname = document.getElementById("lastname");
+        let email = document.getElementById("email");
+        let message = document.getElementById("message");
+
+        // preparing return values from form
+        const visitorName = "Sender : " + firstname.value + " " + lastname.value;
+        const visitorEmail = "E-mail address : " + email.value;
+        const visitorMsg = "Message : " + message.value;
+
+        // displaying return to the console
+        console.log(visitorName);
+        console.log(visitorEmail);
+        console.log(visitorMsg);
+
+        // resetting form
+        firstname.value = "";
+        lastname.value = "";
+        email.value = "";
+        message.value = "";
+
+        // closing form
+        this.closeModal();
+    }
 }
