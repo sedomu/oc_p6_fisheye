0cc2a6a34781dda17e07211e73b5cf682bfa78f7
sedomu <seguin@lilo.org>
2025-03-11 14:59:22 +0100

    update jsdoc lightboxComponent.js

--- scripts/components/lightboxComponent.js
+++ scripts/components/lightboxComponent.js
@@ -1,4 +1,23 @@
+/**
+ * Manages the lightbox modal for viewing media such as images and videos.
+ * Handles opening, closing, and navigating between media items within the lightbox.
+ *
+ * @class Lightbox
+ * @property {boolean} modalState - The current state of the modal (open or closed).
+ * @property {HTMLElement} element - The background element for the lightbox modal.
+ * @property {HTMLElement} close - The button to close the lightbox modal.
+ * @property {HTMLElement} viewer - The viewer area within the modal where the media is displayed.
+ * @property {HTMLElement} previous - The button to view the previous media item.
+ * @property {HTMLElement} next - The button to view the next media item.
+ * @property {HTMLElement} heading - The title of the media being displayed in the lightbox.
+ * @property {HTMLElement} body - The body of the page, used to control scroll behavior when the modal is open.
+ * @property {number} currentItem - The index of the current media item being viewed.
+ * @property {NodeList} medias - A collection of media elements on the page that can be displayed in the lightbox.
+ */
 class Lightbox {
+    /**
+     * Initializes the lightbox by setting up event listeners for opening, closing, and navigation controls.
+     */
     constructor() {
         this.modalState = false;
 
@@ -15,10 +34,6 @@ class Lightbox {
             this.handleNextMedia()
         });
 
-
-        // this.previousE = null;
-        // this.nextE = null;
-
         this.heading = document.querySelector('.lightbox-modal__title');
         this.close.addEventListener('click', () => {
             this.closeModal()
@@ -26,8 +41,6 @@ class Lightbox {
         this.body = document.querySelector("body");
 
         this.currentItem = null;
-        // this.medias = document.querySelectorAll(".media-card__media > *");
-
 
         // Keyboard controls
         document.addEventListener('keyup', (e) => {
@@ -51,71 +64,56 @@ class Lightbox {
 
         document.addEventListener("click", (e) => {
             if (e.target.classList.contains("media-card__media-object")) {
-                console.log(e.target.getAttribute("e-number"))
                 this.handleOpenMedia(e.target.getAttribute("e-number"));
             }
         })
-
     }
 
+    /**
+     * Closes the lightbox modal and restores page scrolling.
+     */
     closeModal() {
         this.element.style.display = 'none';
         this.body.style.overflowY = 'auto';
         this.modalState = false;
     }
 
+    /**
+     * Opens the lightbox modal and disables page scrolling.
+     */
     openModal() {
         this.element.style.display = 'block';
         this.body.style.overflowY = 'hidden';
         this.modalState = true;
     }
 
+    /**
+     * Displays the currently selected media in the lightbox viewer.
+     *
+     * @param {HTMLElement} media - The media element to display in the lightbox viewer.
+     */
     displayOpenMedia(media) {
-
-        console.log(media);
-
         this.viewer.replaceChild(media, this.viewer.firstChild);
         this.heading.innerText = media.title;
         this.openModal();
     }
 
-    handleControls(i, medias) {
-        // if (this.previousE && this.nextE){
-        //     this.previous.removeEventListener('click', this.previousE);
-        //     document.removeEventListener("keyup", (e) => {
-        //         if (e.key === "ArrowLeft"){
-        //             this.previousE();
-        //         } else if (e.key === "ArrowRight"){
-        //             this.nextE()
-        //         }
-        //
-        //     })
-        // }
+    /**
+     * Handles the navigation controls (previous/next) for the lightbox.
+     *
+     * @param {number} i - The index of the current media item.
+     * @param {NodeList} medias - The list of all media elements available for navigation.
+     */
 
-        // this.previousE = () => {this.handleOpenMedia(i > 0 ? i-1 : medias.length - 1, medias);};
-        // this.nextE = () => {this.handleOpenMedia(i < medias.length - 1 ? i+1 : 0, medias);};
-        //
-        // // mouse navigation
-        // this.previous.addEventListener('click', this.previousE);
-        // this.next.addEventListener('click', this.nextE);
-        // // keyboard navigation
-        // document.addEventListener("keyup", (e) => {
-        //     if (e.key === "ArrowLeft"){
-        //         this.previousE();
-        //     } else if (e.key === "ArrowRight"){
-        //         this.nextE()
-        //     }
-        //
-        // })
-    }
-
+    /**
+     * Opens the media item at the given index and displays it in the lightbox.
+     *
+     * @param {number} i - The index of the media to be displayed.
+     */
     handleOpenMedia(i) {
         this.currentItem = Number(i);
 
         this.medias = document.querySelectorAll(".media-card__media > *");
-
-        console.log(this.currentItem);
-        console.log(this.medias);
 
         const media = document.createElement(this.medias[this.currentItem].tagName);
         if (media.tagName === "VIDEO") {
@@ -127,17 +125,17 @@ class Lightbox {
         this.displayOpenMedia(media);
     }
 
+    /**
+     * Displays the previous media item in the lightbox.
+     */
     handlePreviousMedia() {
         this.handleOpenMedia((this.currentItem === 0) ? this.medias.length - 1 : this.currentItem - 1);
     }
 
+    /**
+     * Displays the next media item in the lightbox.
+     */
     handleNextMedia() {
         this.handleOpenMedia((this.currentItem === this.medias.length - 1) ? 0 : this.currentItem + 1);
     }
-
-    // openMedia(){
-    //     for (let i = 0; i < this.medias.length; i++) {
-    //         this.medias[i].addEventListener("click", () => {this.handleOpenMedia(i)})
-    //     }
-    // }
 }
